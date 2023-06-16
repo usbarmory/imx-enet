e073d044c096905d1e35a91d251233d16a84105c
Andrea Barisani <andrea@inversepath.com>
2023-06-16 10:40:23 +0200

    align to new TamaGo networking

--- net.go
+++ net.go
@@ -97,6 +97,7 @@ func (iface *Interface) configure(mac string, ip tcpip.AddressWithPrefix, gw tcp
 	})
 
 	iface.Stack.SetRouteTable(rt)
+	iface.hookGoNet()
 
 	return
 }
@@ -171,13 +172,15 @@ func (iface *Interface) DialContextTCP4(ctx context.Context, address string) (ne
 // DialUDP4 creates a UDP connection to the ip:port specified by rAddr, optionally setting
 // the local ip:port to lAddr.
 func (iface *Interface) DialUDP4(lAddr, rAddr string) (net.Conn, error) {
-	rFullAddr, err := fullAddr(rAddr)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse rAddr %q: %v", rAddr, err)
-	}
-
+	var rFullAddr tcpip.FullAddress
 	var lFullAddr tcpip.FullAddress
+	var err error
+
+	if rAddr != "" {
+		if rFullAddr, err = fullAddr(rAddr); err != nil {
+			return nil, fmt.Errorf("failed to parse rAddr %q: %v", rAddr, err)
+		}
+	}
 
 	if lAddr != "" {
 		if lFullAddr, err = fullAddr(lAddr); err != nil {
