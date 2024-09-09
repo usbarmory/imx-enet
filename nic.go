21a2379f999a51131b74bf3f795c00abc7806edf
Andrea Barisani <andrea@inversepath.com>
2024-09-09 23:36:58 +0200

    bump dependencies

--- nic.go
+++ nic.go
@@ -91,9 +91,9 @@ func (eth *NIC) Rx(buf []byte) {
 
 // Tx transmits a single Ethernet frame to the virtual Ethernet instance.
 func (eth *NIC) Tx() (buf []byte) {
-	var pkt stack.PacketBufferPtr
+	var pkt *stack.PacketBuffer
 
-	if pkt = eth.Link.Read(); pkt.IsNil() {
+	if pkt = eth.Link.Read(); pkt == nil {
 		return
 	}
 
